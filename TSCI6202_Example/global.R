8e33ffaaacaf002c9e2ffe1a95d0e3aa38fc16aa
Nie <NieJ@win.uthscsa.edu>
2022-02-24 13:42:27 -0600

    0224

--- TSCI6202_Example/global.R
+++ TSCI6202_Example/global.R
@@ -5,10 +5,10 @@ library(ggplot2)
 #install_formats()
 
 if(!file.exists("cached_data.tsv") || as.Date(file.info('cached_data.tsv')$ctime) < Sys.Date() ) {
-  
+
   dat0 <- fromJSON(txt = 'https://services.arcgis.com/g1fRTDLeMgspWrYp/arcgis/rest/services/SAMHD_DailySurveillance_Data_Public/FeatureServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&outSR=4326&f=json')
-  
-  dat1 <- dat0[["features"]][["attributes"]] %>% subset(!is.na(total_case_cumulative)) %>% 
+
+  dat1 <- dat0[["features"]][["attributes"]] %>% subset(!is.na(total_case_cumulative)) %>%
     mutate(reporting_date = as.POSIXct(reporting_date/1000, origin = "1970-01-01"))
   # export(dat1, "cached_data.tsv")
   write.table(dat1, file='cached_data.tsv', quote=FALSE, na = "",sep='\t')
